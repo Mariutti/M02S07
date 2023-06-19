6ff0f0d68c1b9ced1557d78fcda41122d79a8896
Pedro Alencar <pedromariutti@gmail.com>
2023-06-19 18:16:50 -0300

    Commit da questão 01 do exercício da semana

---
+++ M02S07.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE exercicio_sem07;
+
+--1 Crie uma tabela chamada clientes, com os seguintes atributos:
+--  Id
+--  Nome
+--  Telefone
+--  Endereco
+CREATE TABLE Clientes (
+	id iNT,
+  	Nome VARCHAR(30),
+  	Telefone VARCHAR(11),
+  	Endereco VARCHAR(150)
+);
+
+
+SELECT * FROM Clientes;
