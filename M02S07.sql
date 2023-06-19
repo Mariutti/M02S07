77a6d2aa4ac3f7fa18ce2519a84ffb67fcd07186
Pedro Alencar <pedromariutti@gmail.com>
2023-06-19 18:23:45 -0300

    Commit da questão 02 do exercício da semana

--- M02S07.sql
+++ M02S07.sql
@@ -12,5 +12,15 @@ CREATE TABLE Clientes (
   	Endereco VARCHAR(150)
 );
 
+--2 Crie um script para inserir os seguintes clientes:
+--  Id: 1, Nome: Vinicius Silva , Telefone: 987654, Endereco: Rua Girassol
+--  Id: 2, Nome: Maria Antonia , Telefone: 123456 , Endereco: Rua Rosas
+--  Id: 3, Nome: Marcus Vinicius , Telefone: 654123, Endereco: Rua Itajai
+INSERT INTO Clientes  (ID, Nome, Telefone, Endereco)
+	VALUES
+    	(1, 'Vinicius Silva' , '987654', 'Rua Girassol'),
+        (2, 'Maria Antonia' , '123456' , 'Rua Rosas'),
+        (3, 'Marcus Vinicius' , '654123', 'Rua Itajai');
+
 
 SELECT * FROM Clientes;
