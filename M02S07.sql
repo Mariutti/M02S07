8998ae8a49a0c3b58a661e82e17f52e1127c311c
Pedro Alencar <pedromariutti@gmail.com>
2023-06-19 18:38:52 -0300

    Commit da questão 08 do exercício da semana

--- M02S07.sql
+++ M02S07.sql
@@ -43,3 +43,63 @@ WHERE Nome = 'Marcus Vinicius';
 --  Atenção: Para não rodar um delete sem WHERE
 DELETE FROM Clientes
 WHERE id = 2;
+
+--8 Com base na seguinte tabela de funcionarios realize a normalização dela  até sua terceira forma normal e crie todos os scripts de criação das tabelas e inclusão dos registros
+CREATE TABLE Funcionarios (
+  Id INT,
+  Nome VARCHAR(15),
+  Cargo VARCHAR(20),
+  tel1 VARCHAR(11),
+  tel2 VARCHAR(11)
+);
+
+INSERT INTO Funcionarios
+	VAlUES
+    	(1, 'Marcos', 'Atendente', '3654589', '36545987'),
+        (2, 'Maria', 'Gerente', '3654689', '36524570'),
+        (3, 'Julia', 'Atendente', '3654962', '12365458');
+SELECT * FROM Funcionarios;
+
+--1FN
+--Selecionar Chave Primária da Tabela Principal;
+--Retirar da tabela colunas de valores múltiplos
+--Subdividir colunas com valores compostosdemo
+ALTER TABLE funcionarios
+	DROP tel1,
+    DROP tel2,
+    ADD PRIMARY KEY (Id);
+    
+CREATE TABLE Telefones (
+	Id_Funcionario INT,
+	tel VARCHAR (11),
+ 	PRIMARY KEY(Id_Funcionario, tel),
+  	CONSTRAINT fk_id_funcionario
+  		FOREIGN KEY(Id_Funcionario) 
+		REFERENCES Funcionarios(Id)  
+);
+
+INSERT INTO telefones
+	VAlUES
+    	(1, '3654589'),
+        (1, '36545987'),
+        (2, '3654689'), 
+        (2, '36524570'),
+        (3, '3654962'), 
+        (3, '12365458');
+
+SELECT * FROM Telefones;
+
+
+--2FN
+--Estar na 1FN
+--No caso de chaves primárias compostas, verificar dependências funcionais parciais de atributos não chave e separá-las em outras tabelas
+  
+--A tabela Funcionario não apresenta chave primária composta, então, automaticamente, já pode ser considerada dentro da 2FN.
+--A tabela Telefones, embora possua chave primária composta, não possui atributos não chave, não havendo como ocorrer dependência funcional parcial.
+
+  
+--3FN
+--Estar nas 1FN e 2FN
+--Não apresentar dependência funcional transitiva.
+
+--As tabelas não apresentam atributos não chave dependendo de outros atributos não chave, estando, portanto, nas formas 3FN já a partir da 1FN.
